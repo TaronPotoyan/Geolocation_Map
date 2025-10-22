52c9ca6d915e4fd990346847ce5a138c5e12c047
Taron Potoyan <potoyantaron@gmail.com>
2025-10-22 13:01:56 +0400

    asd

--- script.js
+++ script.js
@@ -1,37 +1,12 @@
-"use strict";
+'use strict';
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector(".form");
-const containerWorkouts = document.querySelector(".workouts");
-const inputType = document.querySelector(".form__input--type");
-const inputDistance = document.querySelector(".form__input--distance");
-const inputDuration = document.querySelector(".form__input--duration");
-const inputCandence = document.querySelector(".form__input--candence");
-const inputElevation = document.querySelector(".form__input--elevation");
-
-if (navigator.geolocation.getCurrentPosition !== undefined) {
-  console.log("is not undefined");
-  navigator.geolocation.getCurrentPosition(GeoSucces, GeoFail);
-}
-function GeoSucces(positon) {
-  const { latitude } = positon.coords;
-  const { longitude } = positon.coords;
-}
-
-function GeoFail() {
-  alert("Could not get yout positon");
-}
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
